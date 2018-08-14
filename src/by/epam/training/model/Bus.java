c54e2bad97dc1bffdc7ec8a13625b3c96762eac1
Max.Burishinets <arhor@gmail.com>
2018-08-14 22:08:18 +0300

    little bit refactor

--- src/by/epam/training/model/Bus.java
+++ src/by/epam/training/model/Bus.java
@@ -9,76 +9,75 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Bus extends Thread {
-	
-	private static final Logger LOG = LogManager.getLogger(Bus.class);
-	
-	private int busNumber;
-	private int passengers;
-	private final int MAX_CAPACITY = 25;
-	private List<Station> route = new ArrayList<>();
-	private Exchanger<Integer> exchanger;
 
-	@Override
-	public void run() {
-		int n = route.size();
-		for (int i = 0; i < 5; i++) {
-			int stationNumber = i % n;
-			Station current = route.get(stationNumber);
-			current.connect(this);
-			try {
-				TimeUnit.MILLISECONDS.sleep((int)(Math.random() * 300 + 300)); // on the way...
-			} catch (InterruptedException e) {
-				LOG.error("Interrupted exception occured", e);
-			}
-		}
-	}
-	
-	// passengers leave the bus
-	public int exit() {
-		int leaving = (int)(Math.random() * passengers + 0.5);
-		passengers -= leaving;
-		return leaving;
-	}
-	
-	// passengers enter the bus
-	public void enter(int entering) {
-		passengers += entering;
-	}
-	
-	public int getFreeSeats() {
-		return MAX_CAPACITY - passengers;
-	}
+    private static final Logger LOG = LogManager.getLogger(Bus.class);
 
-	public void setBusNumber(int busNumber) {
-		this.busNumber = busNumber;
-	}
-	
-	public int getBusNumber() {
-		return busNumber;
-	}
-	
-	public void setPassengers(int passengers) {
-		this.passengers = passengers;
-	}
-	
-	public int getPassengers() {
-		return passengers;
-	}
-	
-	public void setRoute(List<Station> route) {
-		this.route = route;
-	}
-	
-	public List<Station> getRoute() {
-		return route;
-	}
+    private int busNumber;
+    private int passengers;
+    private final int MAX_CAPACITY = 25;
+    private List<Station> route = new ArrayList<>();
+    private Exchanger<Integer> exchanger;
 
-	public Exchanger<Integer> getExchanger() {
-		return exchanger;
-	}
+    @Override
+    public void run() {
+        int n = route.size();
+        for (int i = 0; i < 5; i++) {
+            int stationNumber = i % n;
+            Station current = route.get(stationNumber);
+            current.connect(this);
+            try {
+                TimeUnit.MILLISECONDS.sleep((int)(Math.random() * 300 + 300)); // on the way...
+            } catch (InterruptedException e) {
+                LOG.error("Interrupted exception occured", e);
+            }
+        }
+    }
 
-	public void setExchanger(Exchanger<Integer> exchanger) {
-		this.exchanger = exchanger;
-	}
+    // passengers leave the bus
+    public int exit() {
+        int leaving = (int)(Math.random() * passengers + 0.5);
+        passengers -= leaving;
+        return leaving;
+    }
 
+    // passengers enter the bus
+    public void enter(int entering) {
+        passengers += entering;
+    }
+
+    public int getFreeSeats() {
+        return MAX_CAPACITY - passengers;
+    }
+
+    public void setBusNumber(int busNumber) {
+        this.busNumber = busNumber;
+    }
+
+    public int getBusNumber() {
+        return busNumber;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public void setRoute(List<Station> route) {
+        this.route = route;
+    }
+
+    public List<Station> getRoute() {
+        return route;
+    }
+
+    public Exchanger<Integer> getExchanger() {
+        return exchanger;
+    }
+
+    public void setExchanger(Exchanger<Integer> exchanger) {
+        this.exchanger = exchanger;
+    }
 }
--- src/by/epam/training/model/Station.java
+++ src/by/epam/training/model/Station.java
@@ -12,96 +12,95 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Station {
-	
-	private static final Logger LOG = LogManager.getLogger(Station.class);
-	
-	private String name;
-	private int waiting;
-	private final Semaphore semaphore = new Semaphore(2, true);
-	private final Exchanger<Integer> ex = new Exchanger<Integer>();
-	private final Lock locking = new ReentrantLock();
-	private final Condition condition = locking.newCondition();
-	
-	public void connect(Bus bus) {
-		try {
-			semaphore.acquire();
-			bus.setExchanger(ex);
-			int leaving = bus.exit();
-			// TODO: implement exchange between two buses
-			if (bus.getPassengers() > 0) {
-				int swap = -1;
-				try {
-					swap = ex.exchange(bus.getPassengers(), 
-							1000,
-							TimeUnit.MILLISECONDS);
-					LOG.info("Было " + bus.getPassengers() 
-					        + " в автобусе № " + bus.getBusNumber()
-					        + " на остановке [" + name + "]\n");
-				} catch (InterruptedException e) {
-					LOG.error("Interrupted exception occured", e);
-				} catch (TimeoutException e) {
-					LOG.debug("There is not bus to swap passengers", e);
-				} finally {
-					if (swap > -1) {
-						bus.setPassengers(swap);
-						LOG.info("Стало " + bus.getPassengers()
-						        + " в автобусе № " + bus.getBusNumber()
-						        + " на остановке [" + name + "]\n");
-					}	
-				}
-			}
-			int freeSeats = bus.getFreeSeats();
-			int entering = getEntering(freeSeats);
-			bus.enter(entering);
-			waiting += leaving;
-			
-		}catch(InterruptedException e) {
-			LOG.error("Interrupted exception occured", e);;
-		} finally {
-			semaphore.release();
-		}
-	}
-	
-	public Station(String name) {
-		setName(name);
-		setWaiting((int)(Math.random() * 100 + 1));
-	}
-	
-	public int getEntering(int freeSeats) {
-		try {
-			locking.lock();
-			int entering = (int)(Math.random() * waiting + 0.5);
-			entering = entering <= freeSeats ? entering : freeSeats;
-			waiting -= entering;
-			return entering;
-		} finally {
-			condition.signal();
-			locking.unlock();
-		}
-		
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setWaiting(int waiting) {
-		this.waiting = waiting;
-	}
-	
-	public int getWaiting() {
-		return waiting;
-	}
-	
-	@Override
-	public String toString() {
-		return getClass().getSimpleName()
-				+ "@"
-				+ getName() + ", "
-				+ getWaiting();
-	}
+
+    private static final Logger LOG = LogManager.getLogger(Station.class);
+
+    private String name;
+    private int waiting;
+    private final Semaphore semaphore = new Semaphore(2, true);
+    private final Exchanger<Integer> ex = new Exchanger<Integer>();
+    private final Lock locking = new ReentrantLock();
+    private final Condition condition = locking.newCondition();
+
+    public void connect(Bus bus) {
+        try {
+            semaphore.acquire();
+            bus.setExchanger(ex);
+            int leaving = bus.exit();
+            if (bus.getPassengers() > 0) {
+                int swap = -1;
+                try {
+                    swap = ex.exchange(bus.getPassengers(), 
+                            1000,
+                            TimeUnit.MILLISECONDS);
+                    LOG.info("Before: " + bus.getPassengers() 
+                            + " in the bus # " + bus.getBusNumber()
+                            + " at station [" + name + "]\n");
+                } catch (InterruptedException e) {
+                    LOG.error("Interrupted exception occured", e);
+                } catch (TimeoutException e) {
+                    LOG.debug("There is not bus to swap passengers", e);
+                } finally {
+                    if (swap > -1) {
+                        bus.setPassengers(swap);
+                        LOG.info(" After: " + bus.getPassengers()
+                                + " in the bus # " + bus.getBusNumber()
+                                + " at station [" + name + "]\n");
+                    }	
+                }
+            }
+            int freeSeats = bus.getFreeSeats();
+            int entering = getEntering(freeSeats);
+            bus.enter(entering);
+            waiting += leaving;
+
+        }catch(InterruptedException e) {
+            LOG.error("Interrupted exception occured", e);;
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    public Station(String name) {
+        setName(name);
+        setWaiting((int)(Math.random() * 100 + 1));
+    }
+
+    public int getEntering(int freeSeats) {
+        try {
+            locking.lock();
+            int entering = (int)(Math.random() * waiting + 0.5);
+            entering = entering <= freeSeats ? entering : freeSeats;
+            waiting -= entering;
+            return entering;
+        } finally {
+            condition.signal();
+            locking.unlock();
+        }
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setWaiting(int waiting) {
+        this.waiting = waiting;
+    }
+
+    public int getWaiting() {
+        return waiting;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName()
+                + "@"
+                + getName() + ", "
+                + getWaiting();
+    }
 }
